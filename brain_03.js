02c4c0041f437cba623b99105b5e76f283029760
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-11-10 16:58:52 -0700

    had to add a containsAll() function to Statement, because, i realized i was evaluating stack members incorrectly.. i need to expand the move's preconditions (not add list), and part of it is that i need to start with a statement which is the complete list born from the move's precondition list. so, i needed a containsAll() function, like i said. shmerg.

--- brain_03.js
+++ brain_03.js
@@ -35,7 +35,9 @@
    */
   function Statement(predicates) {
     this.list = predicates || [];
+    this.statement = true;
     this.containsOne = containsOne;
+    this.containsAll = containsAll;
     this.toString = toString;
     this.add = add;
     this.unpack = unpack;
@@ -59,12 +61,33 @@
       return false;
     }
 
+    function containsAll(s) {
+      for (var j = 0; j < s.list.length; ++j) {
+        var match = false;
+        //console.log('list[' + i + '] is ' + list[i].name);
+        for (var i = 0; i < this.list.length; ++i) {
+          //console.log('s.list[' + j + '] is ' + s.list[j].name);
+          if (this.list[i].equalTo(s.list[j])) {
+            //console.log('found! ' + list[i].name + ' equalTo ' + s.list[j].name);
+            match = true;
+          }
+        }
+        console.log('evaluated one member. match found?: ' + match);
+        console.log(this.list[j]);
+        if (!match) {
+          return false;
+        }
+      }
+      return true;
+    }
+
     /**
      * unpacks a statement into an array of singleton statements
      * @returns {Array} - array of Statements, each containing one predicate
      */
     function unpack() {
-      var output = [];
+      var output = [(new Statement(this.list))];
+      output[0].completeStatement = true;
       for (var i = this.list.length - 1; i >= 0; --i) {
         output.push(new Statement([this.list[i]]));
       }
@@ -170,7 +193,7 @@
   }
 
 
-  function strips(move,ops,members,currentCallback,planCallback,thisPath) {
+  function strips(move,ops,members,thisPath) {
     // starts with a stack which has a move on the top of it
     // expand the move
     var current = currentCallback();
@@ -180,28 +203,34 @@
     //console.log(current);
     //console.log(planCallback());
 
-    // evaluate the stack: for each member in the stack
-    //   is it a move? if we arrived at it by this process, then that means we need to apply it to the current
-    //    also, add the move.name to the 'plan'.
-    //   if not a move, it's a predicate. is it true? if so, pop, repeat.
-    //   if it's false, then generate moves from it and recurse.
     for (var i = (stack.length - 1); i >= 0; --i) {
+      var properlyContained = true;
+      for (var ii = 0; ii < stack[i].list.length; ++ii) {
+        if (stack[i].containsOne(current)) {
+          properlyContained = false;
+        }
+      }
+
       if (stack[i].containsOne(current)) {
-        console.log('found a true predicate: ' + stack[i]);
+        //console.log('found a true predicate: ' + stack[i]);
         // pop any true predicates
         stack.pop();
       } else {
         // we need to generate a move and recurse. hopefully it will result in moving in the right direction..
         var validMoves = ops.generateOperations(stack[i],members);
+        //console.log('valid moves:')
         //console.log(validMoves);
+        // try moves until one works or you run out of moves
         for (var j = 0; j < validMoves.length; ++j) {
           var tryThisMove = validMoves[j];
           var doneBefore = (thisPath.indexOf(tryThisMove.name) > -1);
-          console.log('tested if this was done before. was it? ' + doneBefore);
+          //console.log('tested if this was done before. was it? ' + doneBefore);
           if (!doneBefore) {
             thisPath.push(tryThisMove.name);
-            if (strips(tryThisMove,ops,members,currentCallback,planCallback,thisPath)) {
+            if (strips(tryThisMove,ops,members,thisPath)) {
+              //console.log('list length of delete-able predicates: ' + tryThisMove.d.list.length)
               for (var k = 0; k < tryThisMove.d.list.length; ++k) {
+                //console.log('deleting this guy: ' + tryThisMove.d.list[k]);
                 currentCallback('d', tryThisMove.d.list[k]);
               }
               for (var k = 0; k < tryThisMove.a.list.length; ++k) {
@@ -213,41 +242,37 @@
               j = validMoves.length;  // break this for loop
             }
           }
-          console.log('at end of loop through valid moves, call depth ' + thisPath.length);
-          console.log(currentCallback());
-          console.log(planCallback());
+          //console.log('at end of loop through valid moves, call depth ' + thisPath.length);
+          //console.log(currentCallback().toString());
+          //console.log(planCallback());
         }
       }
     }
-
-    // if we got here, we cleared the stack, aka, the move that we generated this strips() call with can be applied to
+    // if we got here, and we cleared the stack, aka, the move that we generated this strips() call with can be applied to
     // our plan and our current state.
-    if (stack.length == 0) {
-      return true;
-    } else {
-      return false;
-    }
-
-    /* TODO: apply move to plan and current state */
-
+    //console.log('cleared the stack? ' + (stack.length == 0))
+    return (stack.length == 0);
   }
 
 
   // tests
   //var shouldBe = 'should be true: ';
   //var shouldNotBe = 'should be false: ';
-  //var p1 = new Predicate('one', 'A', 'B');
-  //var p2 = new Predicate('two', 'B', 'C');
-  //var p3 = new Predicate('one', 'A', 'B');
+  var p1 = new Predicate('on', 'A', 'B');
+  var p2 = new Predicate('on', 'B', 'A');
+  //var p3 = new Predicate('on', 'A', 'B');
+  var p4 = new Predicate('armempty');
   //console.log(shouldNotBe + p1.equalTo(p2));
   //console.log(shouldBe + p1.equalTo(p1));
   //console.log(shouldBe + p1.equalTo(p3));
-  //var s1 = new Statement([p1, p2]);
-  //var s2 = new Statement([p1]);
-  //var s3 = new Statement([p2]);
+  var s1 = new Statement([p1, p2]);
+  var s2 = new Statement([p1]);
+  var s3 = new Statement([p2]);
+  var s4 = new Statement([p2,p4]);
   //console.log(shouldBe + s1.containsOne(s2));
   //console.log(shouldNotBe + s2.containsOne(s3));
   //
+
   var start = new Statement([
     new Predicate('on', 'B', 'A'),
     new Predicate('ontable', 'A'),
@@ -281,7 +306,8 @@
   }
 
   function currentCallback(flag,body) {
-    console.log('currentCallback(' + flag + ',' + body +')');
+    var stubContents = ((flag==null) ? "" : flag + ',' + body)
+      //console.log('currentCallback(' + stubContents +')');
     if (flag == null) {
       return current;
     } else if (flag == 'a') {
@@ -291,12 +317,17 @@
     }
   }
 
+
+  //console.log(s1.containsAll(s2));
+  //console.log(start.containsAll(s4))
+  //console.log(currentCallback().toString());
+
   //planCallback('abcd');
   //console.log(planCallback());
-  currentCallback('a',new Predicate('jojo'));
-  console.log(currentCallback());
-  currentCallback('d',new Predicate('jojo'));
-  console.log(currentCallback());
+  //currentCallback('a',new Predicate('jojo'));
+  //console.log(currentCallback());
+  //currentCallback('d',new Predicate('jojo'));
+  //console.log(currentCallback());
   //var valid = ops.generateOperations(goal, members);
   //console.log('valid operations:');
   //console.log(valid);
@@ -305,8 +336,6 @@
     goal,
     blocksWorldOperations(),
     members,
-    currentCallback,
-    planCallback,
     []
   );
 
